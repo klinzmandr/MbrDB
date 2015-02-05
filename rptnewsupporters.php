7d505cbdde7dab50ca0c909cf42998249da7334a
klinzmandr <dave.klinzman@yahoo.com>
2015-02-05 13:18:24 -0800

    modified report to output members joined within the date range and list those dues and donations also funded during the date range period

--- rptnewsupporters.php
+++ rptnewsupporters.php
@@ -15,19 +15,14 @@ include 'Incls/seccheck.inc';
 include 'Incls/datautils.inc';
 
 $action = isset($_REQUEST[action]) ? $_REQUEST[action] : '';
-$sd = isset($_REQUEST[sd]) ? $_REQUEST[sd] : date('Y-m-01', strtotime("previous month -2 months"));
-$ed = isset($_REQUEST[ed]) ? $_REQUEST[ed] : date('Y-m-t', strtotime("previous month"));
+$sd = isset($_REQUEST[sd]) ? $_REQUEST[sd] : date('Y-01-01', strtotime(now));
+$ed = isset($_REQUEST[ed]) ? $_REQUEST[ed] : date('Y-m-t', strtotime(now));
 
 if (($sd == "") OR ($ed == "")) $action = '';
 
-// set up intro page	
-print <<<pagePart1
-<div class="container">
+print <<<formPart
+<!-- <div class="container"> -->
 <h3>New Supporters Report&nbsp;&nbsp;&nbsp;<a class="btn btn-sm btn-primary" href="javascript:self.close();">(CLOSE)</a></h3>
-<p>This report is a listing of new members, volunteers or donors that have <b>joined</b> within the following specific date range (default is 3 months).</p>
-<p>Listed members are selected by comparing the &apos;Date Joined&apos; of each member record to the specified date range.  If the &apos;Date Joined&apos; is within the specified date range, it is included in this listing.</p>
-<p>The &apos;Date Joined&apos; is set on introduction of the member into the database.  It can not be changed once established.</p>
-
 <form action="rptnewsupporters.php" method="post">
 Start Date:
 <input type="text" name="sd" id="sd" value="$sd">
@@ -37,14 +32,31 @@ End Date:<input type="text" name="ed" id="ed" value="$ed" >
 </form>
 <!-- <a class="btn btn-primary" href="rptnewsupporters.php?action=continue">Continue</a> -->
 
-pagePart1;
+formPart;
 
+if ($action == '') {
+// set up intro page	
+print <<<pagePart1
+<p>This report is a listing of new contacts, members, volunteers or donors that have <b>joined</b> within the following specific date range (default is THIS year to date).</p>
+<p>Listed supporters are selected by comparing the &apos;Date Joined&apos; of each supporter record to the specified date range. If the &apos;Date Joined&apos; is within the specified date range, it is included in this listing.  Funding records for each selected member are included in the totals if they are also within the specified date range and are marked as &apos;Dues&apos; or a &apos;Donation&apos;.</p>
+<p>The &apos;Date Joined&apos; of the supporter record is set on introduction of the supporter into the database.  It can not be changed once established.</p>
+<script src="jquery.js"></script><script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap-datepicker.js"></script>
+<script src="Incls/bootstrap-datepicker-range.inc"></script>
+</body></html>
+
+pagePart1;
+exit;
+}
 if ($action == 'continue') {
-	$sql = "SELECT * 
-		FROM `members` 
-		WHERE `MemDate` BETWEEN '$sd' AND '$ed' 
- 			AND `Inactive` = 'FALSE' 
-		ORDER BY `MCID`;";
+	$sql= "SELECT `members`.*, `members`.`MemDate`, `members`.`MCID`, `donations`.`MCID` AS `MCID-Funding`, `donations`.`DonationDate`, `donations`.`TotalAmount`, `donations`.`Purpose`, `members`.`MemDate` 
+	FROM `pwcmbrdb`.`donations` AS `donations`, 
+		`pwcmbrdb`.`members` AS `members` 
+	WHERE `donations`.`MCID` = `members`.`MCID` 
+	AND `members`.`MemDate` BETWEEN '$sd' AND '$ed' 
+	AND `donations`.`DonationDate` BETWEEN '$sd' AND '$ed' 
+	AND ( `donations`.`Purpose` = 'donation' OR `donations`.`Purpose` = 'dues' ) 
+	ORDER BY `members`.`MCID` ASC;";
 	$res = doSQLsubmitted($sql);
 	$rowcnt = $res->num_rows;
 	if ($rowcnt == 0) {
@@ -59,51 +71,97 @@ if ($action == 'continue') {
 endPage;
 		exit;
 		}
-	//echo "SQL: $sql<br />";
-	echo "<h5>New supporters in range: $rowcnt</h5>";
-	echo "<a href=\"downloads/Supporters.csv\" download=\"supporters.csv\">DOWNLOAD CSV FILE</a>";
-	echo "<button type=\"button\" class=\"btn btn-xs btn-default\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"Downloaded file contains more fields than shown\nFields separated by semicolon(;)\nText fields are quoted.\"><span class=\"glyphicon glyphicon-info-sign\" style=\"color: blue; font-size: 20px\"></span></button>";
-$fa = array();		// array to capture csv file for download
-// dump in csv format
-	$hdr = 'MCID;MemStatus;MemDate;MCType;Lname;Fname;LabelLine;Org;Address;City;St;Zip;';
-	$hdr .= 'Phone;Email;Notes;LastDonDate;LastDonPurpose;LastDonAmt;LastDuesDate;LastDuesAmt;';
-	$hdr .= "LastCorrDate;LastCorrType\n";
-	$fa[] =  $hdr;
+// process SQL results		
+//	echo "SQL: $sql<br />";
+	$fa = array();					// array to capture csv file for download
+	$mcidarray = array(); 	// array to capture html output
+	$mcidcounts = array(); 	// array to capture counts and totals
 	$translate = array("\\" => ' ', "\n" => ' ', "\t"=>' ', "\r"=>' ', "\"" =>'');
+
+	$duestot = 0; $dontot = 0;
 	while ($r = $res->fetch_assoc()) {
+//		echo '<pre> New member '; print_r($r); echo '</pre>';
 		unset($r[MbrID]);
 		unset($r[TimeStamp]);
 		unset($r[Source]);
 		unset($r[Account]);
+		unset($r[Member]);
 		unset($r[E_Mail]);
+		unset($r[Inactive]);
+		unset($r[Inactivedate]);
+		unset($r[Mail]);
 		unset($r[PaidMemberYear]);
 		unset($r[MasterMemberID]);
 		unset($r[CorrSal]);
 		unset($r[Lists]);
-		unset($r[Member]);
-		unset($r[Inactive]);
-		unset($r[Inactivedate]);
-		unset($r[Mail]);
-		$r[MCID] = "\"$r[MCID]\"";
-		$r[Notes] = strtr($r[Notes], $translate);
-		$r[Notes] = "\"$r[Notes]\"";
-		$l = implode(';',$r);
-		$fa[] = "$l\n";
-		//echo '<pre>supporter '; print_r($r); echo '</pre>';
+		unset($r[LastDonDate]);
+		unset($r[LastDonPurpose]);
+		unset($r[LastDonAmount]);
+		unset($r[LastDuesDate]);
+		unset($r[LastDuesAmount]);
+		unset($r[LastCorrDate]);
+		unset($r[LastCorrType]);
+		
+		$mcidarray[$r[MCID]] = $r;		// save for output
+		if ($r[Purpose] == 'Dues') {
+			$mcidcounts[$r[MCID]][duescount] += 1;
+			$mcidcounts[$r[MCID]][dues] += $r[TotalAmount]; 
+			$duestot += $r[TotalAmount]; }
+		else {
+			$mcidcounts[$r[MCID]][doncount] += 1;
+			$mcidcounts[$r[MCID]][donations] += $r[TotalAmount]; 
+			$dontot += $r[TotalAmount]; }
+		
+		
 		}
-	file_put_contents('downloads/Supporters.csv',$fa);
+//	echo '<pre>mcid '; print_r($mcidarray); echo '</pre>';
+//	echo '<pre>mcid counts '; print_r($mcidcounts); echo '</pre>';
 
-mysqli_data_seek($res, 0);		// reset results to read from top
+/*
+// dump in csv format
+*/
+
+//	create reports
+	$duestot = number_format($duestot);
+	$dontot = number_format($dontot);
+	$rowcnt = count($mcidarray);
+	$hdr = 'MCID;MemStatus;MemDate;MCType;Lname;Fname;Org;Address;City;St;Zip;';
+	$hdr .= "Phone;Email;DuesCt;TotDues;DonCt;TotDonations;Notes\n";
+	$fa[] =  $hdr;
+	echo "<h5>New supporters in date range: $rowcnt with total dues paid of $$duestot and total donations paid of $$dontot</h5>";
+	echo "<a href=\"downloads/Supporters.csv\" download=\"supporters.csv\">DOWNLOAD CSV FILE</a>";
+	echo "<button type=\"button\" class=\"btn btn-xs btn-default\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"Downloaded file contains more fields than shown\nFields separated by semicolon(;)\nText fields are quoted.\"><span class=\"glyphicon glyphicon-info-sign\" style=\"color: blue; font-size: 20px\"></span></button>";
+
 // create table report
 echo '<table class="table">
-	<tr><th>MCID</th><th>MemStatus</th><th>MC Type</th><th>DateJoined</th><th>LastDues</th><th>LastDon</th><th>LabelLine1</th><th>Email Address</th><th>Phone</th><th>Notes</th>';
-while ($r = $res->fetch_assoc()) {
+	<tr><th>MCID</th><th>MemStatus</th><th>MC Type</th><th>DateJoined</th><th>TotDues</th><th>TotDon</th><th>LabelLine1</th><th>Email Address</th><th>Phone</th><th>Notes</th>';
+foreach ($mcidarray as $k => $r) {
 		$r[Notes] = strtr($r[Notes], $translate);
-		echo "<tr><td>$r[MCID]</td><td>$r[MemStatus]</td><td>$r[MCtype]</td><td>$r[MemDate]</td><td>$r[LastDuesAmount]</td><td>$r[LastDonAmount]</td><td>$r[NameLabel1stline]</td><td>$r[EmailAddress]</td><td>$r[PrimaryPhone]</td><td>$r[Notes]</td></tr>";
-		//echo '<pre> New member '; print_r($r); echo '</pre>';
+		
+		$dues = number_format($mcidcounts[$k][dues]);
+		$duescsv = $mcidcounts[$k][dues];
+		$duescsvct = '';
+		if (isset($mcidcounts[$k][duescount])) {
+			$duescsvct = $mcidcounts[$k][duescount];
+			$dues = '(' . number_format($mcidcounts[$k][duescount]) . ')$' . $dues; }
+			
+		$dons = number_format($mcidcounts[$k][donations]);
+		$donscsv = $mcidcounts[$k][donations];
+		$donscsvct = '';
+		if (isset($mcidcounts[$k][doncount])) {
+			$donscsvct = $mcidcounts[$k][doncount];
+			$dons = '(' . number_format($mcidcounts[$k][doncount]) . ')$' . $dons; }
+		echo "<tr><td>$r[MCID]</td><td>$r[MemStatus]</td><td>$r[MCtype]</td><td>$r[MemDate]</td><td align=right>$dues</td><td align=right>$dons</td><td>$r[NameLabel1stline]</td><td>$r[EmailAddress]</td><td>$r[PrimaryPhone]</td><td>$r[Notes]</td></tr>";
+		$r[MCID] = "\"$r[MCID]\"";		// escape for csv output
+		$r[Notes] = "\"$r[Notes]\"";
+		$r[AddressLine] = "\"$r[AddressLine]\"";
+		$fa[] = "$r[MCID];$r[MemStatus];$r[MemDate];$r[MCType];$r[LName];$r[FName];$r[Organization];$r[AddressLine];$r[City];$r[State];$r[ZipCode];$r[PrimaryPhone];$r[EmailAddress];$duescsvct;$duescsv;$donscsvct;$donscsv;$r[Notes]\n";
+//	echo '<pre> New member '; print_r($r); echo '</pre>';
 		}
 	echo '</table>----- End of Report -----';		
+	file_put_contents('downloads/Supporters.csv',$fa); // save csv file
 	}
+// echo '<pre> csv '; print_r($fa); echo '</pre>';
 
 ?>
 </div>  <!-- container -->
