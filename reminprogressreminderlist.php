3b1d2eeb2ed5b9f1538bcb35766c31cf42af2d14
klinzmandr <dave.klinzman@yahoo.com>
2015-05-01 15:35:03 -0700

    only list member status 1 and 2

--- reminprogressreminderlist.php
+++ reminprogressreminderlist.php
@@ -19,7 +19,7 @@ $sql = "SELECT `correspondence`.*, `members`.`MemStatus`, `members`.`Inactive`, 
 	FROM `members`, `correspondence` 
 	WHERE `members`.`MCID` = `correspondence`.`MCID` 
 		AND `correspondence`.`Reminders` IS NOT NULL 
-		AND `members`.`MemStatus` > 0 
+		AND (`members`.`MemStatus` = 1 OR `members`.`MemStatus` = 2) 
 		AND `members`.`Inactive` = 'FALSE' 
 	ORDER BY `correspondence`.`MCID` ASC, `correspondence`.`CORID` ASC;";
 $results = doSQLsubmitted($sql);
@@ -63,17 +63,16 @@ while ($r = $results->fetch_assoc()) {
 $drcount = count($dr);
 
 echo "<div class=\"container\">";
-echo '<h3>Outstanding Reminders&nbsp;&nbsp;<a href="javascript:self.close();" class="btn btn-primary">CLOSE</a></h3>';
-echo "<h4>Listing of $drcount MCID's:</h4>
-	<p>The MCID&apos;s listed are those that have been sent reminders.  The listing provides the sorted total count of reminders that have been sent as well as the date of the last one. An MCID will be removed from this list when one of the following occurs:</p>
+echo '<h3>In-progress Reminders&nbsp;&nbsp;<a href="javascript:self.close();" class="btn btn-primary">CLOSE</a></h3>';
+echo "<h4>Listing of $drcount MCIDs:</h4>
+	<p>The MCIDs listed are those members and volunteers that have been sent reminders.  The listing provides the sorted total count of reminders that have been sent as well as the date of the last one. An MCID will be removed from this list when one of the following occurs:</p>
 	<ol>
-	<li>A funding payment is received and entered as a &apos;Dues&apos; payment,</li>
-	<li>The member record is marked as &apos;Inactive&apos;,</li>
-	<li>The status of the member record is marked as &apos;0-Contacts&apos;, or</li>
-	<li>The status of the member record is marked as &apos;3-Donor&apos;.</li>
+	<li>A funding payment is received and entered as a &apos;Dues&apos; payment (or a donation for a 3-Donor),</li>
+	<li>The member record is marked as &apos;Inactive&apos; or</li>
+	<li>The status of the member record is marked as &apos;0-Contacts.</li>
 </ol>";
 echo "<table border=\"0\" class=\"table table-condensed\">";
-echo "<tr><th>MCID</th><th align=\"center\">Name</th><th align=\"center\">Count</th><th>Last Sent</th><th align=\"center\">Type Sent</th></tr>";
+echo "<tr><th>MCID</th><th align=\"center\">Name</th><th>MemStatus</th><th align=\"center\">Count</th><th>Last Sent</th><th align=\"center\">Type Sent</th></tr>";
 
 // ksort($dr);					// sorted by MCID
 arsort($ar);							// sorted by reminder cound
@@ -90,7 +89,7 @@ print <<<bulletForm
 <form action="mbrinformation.php" method="post">
 <input type="radio" name="filter" id="1" value="$mcid" onclick='this.form.submit()'></td></form>
 </td> -->
-<td>$mcid</td><td>$name</td><td>$count</td><td>$ls</td><td>$ct</tr>
+<td>$mcid</td><td>$name</td><td align="center">$ms</td><td>$count</td><td>$ls</td><td>$ct</tr>
 
 </div>  <!-- container -->
 bulletForm;
