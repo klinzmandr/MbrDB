0592cdd900e1fc9df985eed9db968e96a5077597
klinzmandr <dave.klinzman@yahoo.com>
2016-02-14 17:06:53 -0800

    modified payments tab to use new payments programs

--- devscripts.php
+++ devscripts.php
@@ -31,10 +31,10 @@ print <<<tabPage
 
 <div id="myTabContent" class="tab-content">
 <div class="tab-pane fade active in" id="usage">
-<p>This page is to provide all available scripts developed for use by the phone soliciation person to be used during the course of a conversation with the active MCID.  Use the &apos;MCID Lookup&apos; function of the main menu to estalish an &apos;active&apos; MCID.  Contact infomation about the &apos;active&apos; MCID is available on the Info tab of the main menu.  Funding and Correspondence tabs provide historical funding support and member contact information.</p>
+<p>This page is to provide all available scripts developed for use by the phone soliciation person to be used during the course of a conversation with the active MCID.  Use the &apos;MCID Lookup&apos; function of the main menu to estalish an &apos;active&apos; MCID.  Contact information about the &apos;active&apos; MCID is available on the Info tab of the main menu.  Funding and Correspondence tabs provide historical funding support and member contact information.</p>
 <p>Various scripts are developed, usually, by professional Fund Development staff.  Once approved, they are they loaded into the system and become available on the various tabs of this page.  Techniques on phone protocols, questions, tone of voice and other non-visual indicators will largely determine the success or failure of phone solicitation efforts.  These scripts must be developed with an understanding of these factors.</p>
 <p>In general, each tab is free form in nature.  Loading of information for each tab is done after it has been developed and trial tested within the organzation.  Special system administration functions are provided to allow these scripts to be loaded when they are have been developed or updated.
-<p>This page opens up as a seperate tab or window.  All scripts are a part of the page downloaded so that navigation between them is very fast.  Click the appropriate tab to begin.</p>
+<p>This page opens up as a separate tab or window.  All scripts are a part of the page downloaded so that navigation between them is very fast.  Click the appropriate tab to begin.</p>
 <p>Please enter all donor funding activity on the 'Funding' tab and all contacts made on the 'Correspondence' tab.</p>
 </div>
 <div class="tab-pane fade" id="tab1">
@@ -42,7 +42,7 @@ print <<<tabPage
 <ul>
 <p>Mission Statement:</p>
 <p>Organizational Goals:</p>
-<p>Accomplishements:</p>
+<p>Accomplishments:</p>
 </ul>
 </div>  <!-- tab-pane -->
 <div class="tab-pane fade" id="tab2">
@@ -70,10 +70,24 @@ print <<<tabPage
 <div class="tab-pane fade" id="tab4">
 <h4>Payment options include:</h4>
 <ul>
-<li><p>On-line membership payment or donation on our web site or through PayPal</p></li>
-<li><p>Setting up an automatic payment with your bank (would you like more iformation?)</p>
-<li><p>Sending a check or money order directly (would you like the address or have me send a self addressed return remittence envelope?)</p>
+<li>Immediate support by providing payment information right now.</li>
+<li>On-line membership payment or donation on our web site or through PayPal</li>
+<li>Setting up an automatic payment with your bank (would you like more iformation?)
+<li>Sending a check or money order directly (would you like the address or have me send a self addressed return remittence envelope?)
 </ul>
+<h4>Immediate Credit Card and PayPal payment options</h4>
+<ul>
+<li>Credit card information should be considered as sensitive and never recorded or written down.</li>
+<li>The member should ALWAYS be offered the option of:</li>
+<ul><li>being sent an email with the web site link and entering the payment information themselves.-OR-</li>
+<li>being sent a regular mail letter with a return remittance envelope.</li></ul>
+</ul>
+<a href="http://dev.pacwilica.org/CCard/auth.php?desc='Solicitation One-time Donation' " class="btn btn-primary" target="_blank">Credit Card One<br>Time Donation</a>&nbsp;&nbsp;
+<a href="http://dev.pacwilica.org/ARBill/arbcreate.php" class="btn btn-primary" target="_blank">Credit Card Monthly<br>Recurring Donation</a>&nbsp;&nbsp;
+<a href="http://dev.pacwilica.org/PPal/pp.php?enteramt&ddown" class="btn btn-primary" target="_blank">PayPal Payment<br>Options</a>
+<h4>Payment Confirmation</h4>
+<p>Use the mail and/or email facilities in the &apos;Summary&apos; tab of a supporter&apos;s information page to send appropriate follow up messages.</p>
+<!-- <a href='#' class="btn btn-primary" target='_blank'>Send Confirmation Email</a> -->
 </div>  <!-- tab-pane -->
 <div class="tab-pane fade" id="tab5">
 <p>Counters to declinations:</p>
