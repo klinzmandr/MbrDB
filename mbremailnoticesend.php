70fcf038d239c100b0ef6f2d68433608e2474b97
klinzmandr <dave.klinzman@yahoo.com>
2016-05-26 09:35:30 -0700

    write msgs to send queue

--- mbremailnoticesend.php
+++ mbremailnoticesend.php
@@ -16,72 +16,67 @@ include 'Incls/datautils.inc.php';
 
 $mcid = $_SESSION['ActiveMCID'];
 
-function clickable($string){
-  // if anchors already exist - don't translate
-  if (stripos($string,'<a ') !== FALSE) return($string); 
-  // make sure there is an http:// on all URLs
-  $string = preg_replace("/([^\w\/])(www\.[a-z0-9\-]+\.[a-z0-9\-]+)/i", "$1http://$2",$string);
-  //echo "string: $string<br>";
-  // make all URLs links
-  $string = preg_replace("/([\w]+:\/\/[\w-?&;#~=\.\/\@]+[\w\/])/i","<A target=\"_blank\" href=\"$1\">$1</A>",$string);
-  // make all emails hot links
-  $string = preg_replace("/([\w-?&;#~=\.\/]+\@(\[?)[a-zA-Z0-9\-\.]+\.([a-zA-Z]{2,3}|[0-9]{1,3})(\]?))/i","<A HREF=\"mailto:$1\">$1</A>",$string);
-  return $string;
-	}
-
 $to = $_REQUEST['to'];
-$from = $_REQUEST['from'];
+$sender = $_REQUEST['from'];
 $subject = $_REQUEST['subject'];
 $body = $_REQUEST['body'];
 
-$body = clickable($body); // turn url's into links
+list($mcid, $emaddr) = explode(':',$to);
+//echo "emaddr: $emaddr<br>";
+$emh = htmlentities($emaddr);
+$emarrayin[] = $mcid . ': ' . $emaddr;
+//echo "emh: $emh<br>";
+
+$sufrom = $_SESSION['SessionUser'];
+$sql = "SELECT `MCID` FROM `adminusers` WHERE `UserID` = '$sufrom'";
+$res = doSQLsubmitted($sql);
+$r = $res -> fetch_assoc();
+$fromMCID = $r['MCID'];
+//$echo "sufrom: $sufrom, fromMCID: $fromMCID<br>";
 
 $trans = array("\\" => ' ', "\n" => ' ', "\t"=>' ', "\r"=>' ');
-$trsub = strtr($subject, $trans);
-$trmsg  = strtr($body, $trans);
+$subject = strtr($subject, $trans);
+$message  = strtr($body, $trans);
 
+echo '<div class="container">
+<h3>Email Send Confirmation</h3>
+<a class="btn btn-primary" href="mbrinfotabbed.php">RETURN</a>
+<br><br><strong>To: </strong>'.$emh.'<br>
+<strong>From: </strong>'.$sender.'<br>
+<strong>Subject:</strong><br>' . $subject . '<br>
+<strong>Message:</strong><br>' . $message . '<br>';
 
-echo "<div class=\"container\">";
-echo "<h3>Email Send Confirmation</h3>";
-echo "<a class=\"btn btn-primary\" href=\"mbrinfotabbed.php\">RETURN</a>";
+// format email message and write to queue
+$tce = 1;
+$subject = $subject . '  (' . $fromMCID . ')';
+$prefix = date('YmdHis');
+$listname = "../MailQ/$prefix.$tce.LIST";
+$msgname  = "../MailQ/$prefix.$tce.MSG";
+//create message string for output
+$msgarray[] = $sender; $msgarray[] = $subject; $msgarray[] = $message;
+$listval = "Original list size: $tce";
+//echo "list: $listname, msg: $msgname, lock: $lockname<br>";
+sort($emarrayin);
+file_put_contents($listname, implode("\n", $emarrayin));
+file_put_contents($msgname, implode("\n", $msgarray));
 
-echo "<br><br><strong>To: </strong>$to<br>";
-echo "<strong>From: </strong>$from<br>";
-echo "<strong>Subject:</strong> ";
-echo $trsub;
-echo "<br><br><strong>Message:</strong><br>";
-echo $trmsg;
-
-// format email message
-$subject = "PWCMbr: " . $trsub;
-
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= "From: " . $from . "\r\n";
-$headers .= "Reply-To: " . $from . "\r\n";
-$headers .= "Return-Path: " . $from . "\r\n";   // these two to set reply address
-$foption = "-f" . $from;												// notify of undeliverable mail to sender
-
-$finmsg = "";
-$finmsg = $trmsg;
-//$finmsg .= "<br><br><font size=1><center><a href=".$HomeURL."/unsubscribenew.php?unsubscribe=";
-//$finmsg .= $to;
-//$finmsg .= ">Unsubscribe from further PWC e-mail.</a></center></font>";
-$finmsg = wordwrap($finmsg);
-echo date("r") . ": Sent To: $to<br>";
-$tolist .= $to . "\n";
-$mresp = mail($to, $subject, $finmsg, $headers, $foption);
-if ($mresp == FALSE) {
-	echo "ERROR: an error was returned when sending the email message<br />";
-	}
-//echo "<h2>TEST MODE ENABLED - reminder noted - but NO email sent<br /></h2>";
+echo 'server: ' . $_SERVER['SERVER_NAME'] .'<br>';
+echo 'Message written to the send queue.<br>';
+if ($_SERVER['SERVER_NAME'] != 'localhost') {
+  echo '<br>Starting sender program at ' . date('r') . '<br>';
+  // kick the mailsender routine on its way 
+  // cron will automatically schedule every hour, but this gets it start right now
+  // output of command will be in mailsenderlog.txt
+  $cmd = '/home/pacwilica/bin/mailsender';
+  exec($cmd . " > /home/pacwilica/public_html_apps/mailsenderlog.txt &");
+  }
 
 // finally add new correspondence record noting send of this email
 $fields[CorrespondenceType] = 'EmailNotice';
 $fields[DateSent] = date('Y-m-d');
 $fields[MCID] = $mcid;
 $fields[Reminders] = 'EMailNotice';
-$fields[Notes] = "Subject: $trsub";
+$fields[Notes] = "Subject: $subject";
 sqlinsert('correspondence', $fields);
 
 // update member summary info
