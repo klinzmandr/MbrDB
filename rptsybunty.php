8ad3ad585fcb94c72e1ea9b607828daec5fdc408
klinzmandr <dave.klinzman@yahoo.com>
2016-09-22 22:41:32 -0700

    added more years in selection menu

--- rptsybunty.php
+++ rptsybunty.php
@@ -99,9 +99,12 @@ Select the Year: <br>
 <form action="rptsybunty.php">
 <input type="hidden" name="action" value="rpt">
 <select name=yr>
+<option value=2020>2020</option>
+<option value=2019>2019</option>
+<option value=2018>2018</option>
 <option value=2017>2017</option>
-<option value=2016>2016</option>
-<option value=2015 selected>2015</option>
+<option value=2016 selected>2016</option>
+<option value=2015>2015</option>
 <option value=2014>2014</option>
 <option value=2013>2013</option>
 <option value=2012>2012</option>
