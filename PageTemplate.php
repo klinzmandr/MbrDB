bff2df3b754e6abfb66dd5025a747fe4ca47f1e0
klinzmandr <dave.klinzman@yahoo.com>
2017-07-28 15:03:09 -0700

    update with jquery templates included

--- PageTemplate.php
+++ PageTemplate.php
@@ -2,19 +2,33 @@
 <html>
 <head>
 <title>Page Title</title>
+<meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Bootstrap -->
-<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="css/bootstrap.min.css" rel="stylesheet" media="all">
 </head>
 <body>
 <script src="jquery.js"></script>
 <script src="js/bootstrap.min.js"></script>
 
+<script>
+// initial setup of jquery function(s) for page
+$(document).ready(function () {
+	alert(" example of action on document load");
+
+// this attaches an event to an object
+	$("h3").click(function () {
+    alert("example of a click of any header 3 like the page title"); 
+    });
+
+  });  // end ready function
+</script>
+
 <?php
 session_start();
 //include 'Incls/vardump.inc.php';
-include 'Incls/seccheck.inc.php';
-include 'Incls/mainmenu.inc.php';
+//include 'Incls/seccheck.inc.php';
+//include 'Incls/mainmenu.inc.php';
 include 'Incls/datautils.inc.php';
 
 $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
